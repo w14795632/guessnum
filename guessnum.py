f31978b10b25e34880b0f007f90285a8dcf33004
willy jin <willy810110@gmail.com>
2023-05-08 00:33:55 +0800

    final ver

--- guessnum.py
+++ guessnum.py
@@ -1,6 +1,10 @@
 import random
 
-r = random.randint(1, 100)
+start = input("請輸入開始數字: ")
+end = input("請輸入結束數字: ")
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 
 while True:
@@ -9,7 +13,7 @@ while True:
 	num = int(num)
 	if num == r:
 		print("恭喜你猜對了!!")
-		print("這是你猜的第", count, '次')
+		print("這是你猜的第", count, '次')	
 		break
 	elif num > r :
 		print('比答案大喔，在猜一次')
